e692001683e61b9c0f218aabd3b1e4dfa4e117b8
Erbaz <erbazkamran@gmail.com>
2022-04-09 04:19:05 +0500

    route set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import AboutPage from "./pages/about/AboutPage";
+import ContactPage from "./pages/contact/ContactPage";
+import "./pages/home/HomePage"
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>AIC WEBSITE</h1>
-    </div>
+
+      <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route  path='/' element={< HomePage />}></Route>
+        <Route exact path='/about' element={< AboutPage />}></Route>
+        <Route exact path='/contact_us' element={< ContactPage />}></Route>
+      </Routes>
+      </div>
+    
+
   );
 }
 
