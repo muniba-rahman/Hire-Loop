03efe3703b0b6a4d4c8264131af8de073d5375f8
Erbaz <erbazkamran@gmail.com>
2022-04-09 05:40:05 +0500

    redux set up

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import {store} from "./redux/configStore";
 
 createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-
-
+  </Provider>
 );
