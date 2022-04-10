7491f5132c0deaba072f8bae79f16e3e9f64c5de
Erbaz <erbazkamran@gmail.com>
2022-04-11 03:02:57 +0500

    section 2 set up. state management required

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Home.css';
 import image from '../../images/home1.jpg';
+import image2 from '../../images/robotshape-1.png';
+import { FONT_COLOR } from '../../constants/color_constants';
 
 export default function HomePage() {
+  const secTwo = [
+    {
+      type: "About Us",
+      title: "Pioneers of a highly ambitious AI society for the Pakistani youth",
+      para:"Founded in 2016, the AI CLUB is a derivative of the research centre for Artificial intelligence at NEDUET. Established by a group of undergraduate students of NED University, the club is all about artificial intelligence. Integrating several teams having a diverse set of skills, the AI club brings together all the ideas and individuals passionate about AI technology. We hold ourselves true to the promise of enlightening the young enthusiasts and encouraging emerging startups. The AI club organizes various events including; seminars, workshops, and boot camps featuring researchers and innovators shedding light upon topics such as Machine Learning, Deep Learning, data analysis, entrepreneurship, and robotic automation etc."
+    }
+  ]
   return (
     <div>
-        <section className={'sec1'}>
+        <section className={'sec'}>
           <div className={'imageBg'} style={{backgroundImage:`url(${image})`, backgroundSize:'cover', backgroundPosition:'center'}}>
             <div className={'overlay'}>
             <div className={'titlePara'}>
@@ -15,11 +24,26 @@ export default function HomePage() {
               </p>
             </div>
             </div>
-            
           </div>
-          
         </section>
-        <section className={'sec1'}></section>
+        <section className={'sec2'}>
+          <div className={'blockA'}>
+            <div  className={'imageBg2'}>
+              <img src={image2}/>
+            </div>
+            <div style={{flex:1}}>
+              <div className={'blockAB'}>
+                <div style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
+                  <button className= {'typeBtn'}><h3>About Us</h3></button>
+                  <button className= {'typeBtn'}><h3>Our Mission</h3></button>
+                  <button className= {'typeBtn'}><h3>Our Vision</h3></button>
+                </div>
+                <h1 style={{color:FONT_COLOR}}>{secTwo[0].title}</h1>
+                <h3 style={{color:FONT_COLOR}}>{secTwo[0].para}</h3>
+              </div>
+            </div>
+          </div>
+        </section>
     </div>
   )
 }
