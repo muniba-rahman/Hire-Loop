659196b86f38a3e26cd05550f963ba5032a336b9
Erbaz <erbazkamran@gmail.com>
2022-04-10 23:30:56 +0500

    home page - section 1

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import './Home.css';
+import image from '../../images/home1.jpg';
 
 export default function HomePage() {
   return (
     <div>
-        <h1>Home</h1>
+        <section className={'sec1'}>
+          <div className={'imageBg'} style={{backgroundImage:`url(${image})`, backgroundSize:'cover', backgroundPosition:'center'}}>
+            <div className={'overlay'}>
+            <div className={'titlePara'}>
+              <p>
+                <h1>Advancing AI In Pakistan</h1>
+                <h3>We Are Leading The AI Revolution In Pakistan, Educating The The Next Generation Of Pakistani AI Researchers And Innovators</h3>
+              </p>
+            </div>
+            </div>
+            
+          </div>
+          
+        </section>
+        <section className={'sec1'}></section>
     </div>
   )
 }
