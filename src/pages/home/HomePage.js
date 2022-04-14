c2ada070891a1e4ee199aac31218d3022b10a104
Erbaz <erbazkamran@gmail.com>
2022-04-14 06:33:57 +0500

    homePage sections complete, responsive and static

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,10 +3,13 @@ import "./Home.css";
 import image from "../../images/home1.jpg";
 import image2 from "../../images/robotshape-1.png";
 import image3 from "../../images/white-bg.jpg";
+import image4 from "../../images/HowAIWorks.png";
+import image5 from "../../images/white-bg2.jpg";
 import ourProjects1 from "../../images/ourProjects1.jpg";
 import ourProjects2 from "../../images/ourProjects2.jpg";
 import ourProjects3 from "../../images/ourProjects3.jpg";
 import ourProjects4 from "../../images/ourProjects4.jpg";
+import blog1 from "../../images/blog1_mock.jpg";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import {
@@ -52,6 +55,29 @@ export default function HomePage() {
     { image: img_team_register },
     { image: img_team_social },
   ];
+
+  var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+  const blogList = [
+    { 
+      image: blog1,
+      title: 'AI and Psychiatry',
+      date: Date('11/2/2021'),
+      preview: 'WHAT EXACTLY IS PSYCHIATRY? The human psyche and the art of understanding it requires deep complexity as this field has a lot to offer.',
+    },
+    {
+      image: blog1,
+      title: 'SELF DRIVING CARS: PROS AND CONS',
+      date: Date('7/2/2021'),
+      preview: 'NTRODUCTION:   So, there you are on a rainy night, and you have called a taxi. Strangely the driver does not talk. You arrived at'
+    },
+    {
+      image: blog1,
+      title: `Climate Change and AI's Prospects`,
+      date: Date('6/9/2021'),
+      preview: 'Of all the existential threats living species face in the current century and will be facing going into the future, Climate change remains to be'
+    }
+  ]
 
   const renderProjectList = (projects) => {
     return projects.map((obj, index) => {
@@ -149,7 +175,7 @@ export default function HomePage() {
               <h1 style={{ color: FONT_COLOR_DEFAULT }}>
                 {secTwo[buttonIndex].title}
               </h1>
-              <p style={{ color: FONT_COLOR_DEFAULT }}>
+              <p style={{ color: FONT_COLOR_DEFAULT, fontSize: '16px' }}>
                 {secTwo[buttonIndex].para}
               </p>
             </div>
@@ -157,7 +183,7 @@ export default function HomePage() {
         </div>
       </section>
       <section className={"sec sec3"}>
-        <div className={"sec_div"} style={{ color: FONT_COLOR_DEFAULT }}>
+        <div className={"sec_div"}>
           <h1>Our Projects</h1>
           <h3>
             We Work On Ambitious Projects To Create Solutions And Enhance The
@@ -167,13 +193,15 @@ export default function HomePage() {
         <div className={"projList"}>{renderProjectList(ourProjectList)}</div>
       </section>
       <section className={"sec sec4"}>
+      <div className={'navSpace'}></div>
+
         <div
           className={"bgImgSec4"}
           style={{
             backgroundImage: `url(${image3})`,
           }}
         />
-        <div className={"sec_div"} style={{ color: FONT_COLOR_DEFAULT }}>
+        <div className={"sec_div"}>
           <h1>Our Team</h1>
           <h3>
             A Total Of <strong style={{ color: "#44699d" }}>10 Teams</strong>{" "}
@@ -209,7 +237,59 @@ export default function HomePage() {
             })}
           </Carousel>
         </div>
+        <div className={'navSpace'}></div>
+
       </section>
+      <section className={'sec5 sec'}>
+            <div style={{width:'100%', display:'flex', alignItems:"center", flexDirection:'column', justifyContent:'center', textAlign:'center'}}>
+              <h1 style={{width:'80%', textAlign:'center', paddingTop:'5%'}}>How AI Club Works</h1>
+              <h3 style={{ width:'80%',textAlign:'center'}}>The AI Club Brings The Young AI Startups Into The Spotlight Linking Them With Potential Investors.</h3>
+            </div>
+            <div className={'sec5_body'}>
+              <div className={'imageContainer'}>
+                <img src={image4} className={'sec5_img'}/>
+              </div>
+              <div className={'sec5_text'}>
+                <h2 style={{width:'80%', textAlign:'center', paddingTop:'5%'}}>
+                  Preparing undergrad students to dominate the industry by:
+                </h2>
+                <ul className={'listPrepare'}style={{textAlign:'left', width:'80%', fontSize:'16px'}}>
+                  <li>The Annual AI fest provides a medium for sharing and implementing breakthrough ideas.</li><br/>
+                  <li>Arranging seminars, bootcamps, and workshops etc.</li><br/>
+                  <li>Providing access to online courses.</li><br/>
+                </ul>
+              </div>
+            </div>
+      </section>
+      <section className="sec sec6">
+            <div className="navSpace"></div>
+
+            <div
+                className={"bgImgSec6"}
+                style={{
+                  backgroundImage: `url(${image5})`,
+                }}
+            />
+            <div className="sec_div">
+              <h1>Our Blogs</h1>
+            </div>
+            <div className="blogsList"> 
+                {
+                  blogList.map((obj, i)=>{
+                    return  <div key={i} className="blog">
+                    <img src={blog1}/>
+                    <div style={{textAlign:'left'}} >
+                      <h2 className="blogTitle">AI and Psychiatry</h2>
+                      <p className="blogDate">{new Date(blogList[0].date).toLocaleDateString("en-US",dateOptions)}</p>
+                      <p className="blogPreview">{blogList[0].preview}</p>
+                    </div>
+                  </div>
+                  })
+                }
+              </div>
+             
+      </section>
+
     </div>
   );
 }
