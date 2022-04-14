5f16e9adcdacc628353f0d66b4443d6a9a10d009
Erbaz <erbazkamran@gmail.com>
2022-04-14 07:04:37 +0500

    changes in responsiveness

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -111,14 +111,12 @@ export default function HomePage() {
         >
           <div className={"overlay"}>
             <div className={"titlePara"}>
-              <p>
                 <h1>Advancing AI In Pakistan</h1>
                 <h3>
                   We Are Leading The AI Revolution In Pakistan, Educating The
                   The Next Generation Of Pakistani AI Researchers And Innovators
                 </h3>
                 <button className={"joinUsBtn"}>Join Us</button>
-              </p>
             </div>
           </div>
         </div>
