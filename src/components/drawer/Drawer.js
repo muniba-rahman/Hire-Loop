0cd4b336b9efe527bb151936a872f2b964aabc42
Muhammad Erbaz Kamran <merbaz@nisum.com>
2022-04-19 19:41:25 +0500

    drawer in progress

--- src/components/drawer/Drawer.js
+++ src/components/drawer/Drawer.js
@@ -1,10 +1,49 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { drawerToggle } from "../../redux/slices/appStates.slice";
+import { Link } from "react-router-dom";
 import "./Drawer.css";
+import { navRoutes } from "../../constants/route_constants";
 
 const Drawer = ({ isOpen }) => {
   const dispatch = useDispatch();
+
+  const handleDrawerClicked = (e) => {
+    e.stopPropagation();
+  };
+
+  const RenderRouteList = ({ routeList }) => {
+    console.log(routeList);
+    return routeList.map((route, index) => {
+      return (
+        <div key={index} className={"routeItem"}>
+          <Link
+            to={route.path}
+            style={{
+              width: "80%",
+              padding: "8px",
+              textAlign: "left",
+            }}
+          >
+            {route.name}
+          </Link>
+          {route.dropdownRoutes ? (
+            <i
+              className="bi bi-chevron-down"
+              style={{
+                width: "20%",
+                alignSelf: "stretch",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            />
+          ) : null}
+        </div>
+      );
+    });
+  };
+
   return (
     <div
       className={isOpen ? "overlay_open" : "overlay_close"}
@@ -15,7 +54,13 @@ const Drawer = ({ isOpen }) => {
     >
       <div
         className={isOpen ? "drawer_container_open" : "drawer_container_close"}
-      ></div>
+        onClick={handleDrawerClicked}
+      >
+        <div className={"drawer-routeList"}>
+          <div style={{ height: "4vh" }} />
+          <RenderRouteList routeList={navRoutes.default} />
+        </div>
+      </div>
     </div>
   );
 };
